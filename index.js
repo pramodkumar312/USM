be8b1f8dc061ce100b0e2489917531a37710f2b8
parmod <dairupramodkumar@gmail.com>
2023-03-03 07:51:02 +0530

    a

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const databash = require('../config/databash');
+
 //it is use for connect to databash
 const dotenv = require('dotenv').config()
-databash();
+const mongoose =require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/user_system').then(()=>{
+            console.log("databash is connected")});
 const app = require('express')();
 const Route = require('../routes/userRoute');
 app.use('/',Route);
